5b427e820fdd40605b32112572bee0050ced4f43
HappyTanuki <rlaalstn120@gmail.com>
2024-06-27 19:17:30 +0900

    플레이어 컨트롤러 추가함

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -9,6 +9,9 @@ AAuraCharacterBase::AAuraCharacterBase()
  	// Set this character to not call Tick() every frame.  You can turn this on.
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 // Called when the game starts or when spawned
